0e93ca7479f8fd143657bd96bb1e60616830f1eb
felixkkong <felixkkong@hotmail.com>
2018-09-18 19:07:45 +0800

    dvsr

--- LTCTMS/Config.js
+++ LTCTMS/Config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 var config = {
   apiKey: "AIzaSyCnuAZzFvkT-FSRxB5Vk67JM6FU9wZLYMQ",
@@ -9,15 +8,3 @@ var config = {
   messagingSenderId: "323469467975"
 };
 firebase.initializeApp(config);
-=======
-// Initialize Firebase
-    const config = {
-      apiKey: "AIzaSyCnuAZzFvkT-FSRxB5Vk67JM6FU9wZLYMQ",
-      authDomain: "share-b7589.firebaseapp.com",
-      databaseURL: "https://share-b7589.firebaseio.com",
-      projectId: "share-b7589",
-      storageBucket: "share-b7589.appspot.com",
-      messagingSenderId: "323469467975"
-    };
-    firebase.initializeApp(config);
->>>>>>> dde7ef1f47e89d8e9ebbbb86b3261d7e5eabf0c6
