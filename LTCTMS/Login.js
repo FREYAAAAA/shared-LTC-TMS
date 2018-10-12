2eac1b5c4825f549b0a9ddc4e807923924276729
felixkkong <felixkkong@hotmail.com>
2018-10-12 21:43:09 +0800

    login

--- LTCTMS/Login.js
+++ LTCTMS/Login.js
@@ -37,3 +37,109 @@
      }
    });
    });
+
+
+   function bigqr(x) {
+       x.style.height = "100px";
+       x.style.width = "100px";
+   }
+
+   function normalqr(x) {
+       x.style.height = "16px";
+       x.style.width = "16px";
+   }
+
+
+
+
+
+   var cnoaddress = firebase.database().ref("CNO/");
+   console.log("123");
+   var cnousername = []
+   var cnopassword = []
+   var cnoindex = 0;
+   cnoaddress.once("value")
+   .then(function(snapshot){
+     snapshot.forEach(function(first){
+
+     first.forEach(function(second){
+
+       if( second.key == "Portfolio"){
+         second.forEach(function(third){
+
+           if (third.key == "Email"){
+           cnousername[cnoindex] = third.val();
+           console.log(cnousername[cnoindex]);
+         }
+
+           if (third.key == "Password"){
+           cnopassword[cnoindex] = third.val();
+           console.log(cnopassword[cnoindex]);
+
+
+           cnoindex++;
+         }
+       })
+     }
+   })
+   })
+   })
+
+
+
+   var diraddress = firebase.database().ref("DIR/");
+   console.log("123");
+   var dirusername = []
+   var dirpassword = []
+   var dirindex = 0;
+   diraddress.once("value")
+   .then(function(snapshot){
+     snapshot.forEach(function(first){
+
+     first.forEach(function(second){
+
+       if( second.key == "Portfolio"){
+         second.forEach(function(third){
+
+           if (third.key == "Email"){
+           dirusername[dirindex] = third.val();
+           console.log(dirusername[dirindex]);
+         }
+
+           if (third.key == "Password"){
+           dirpassword[dirindex] = third.val();
+           console.log(dirpassword[dirindex]);
+
+
+           dirindex++;
+         }
+       })
+     }
+   })
+   })
+   })
+
+
+function validate(){
+var username = document.getElementById("txtEmail").value;
+var password = document.getElementById("txtPassword").value;
+
+console.log(cnoindex);
+console.log(dirindex);
+
+for(var c=0; c<=cnoindex; c++){
+  console.log("asbviabvdsob")
+if ( username == cnousername[c] && password == cnopassword[c]){
+alert ("Login successfully");
+window.location = "Aboutus.html";
+}
+}
+
+for(var d=0; d<=dirindex; d++){
+console.log("asbviabvdsob")
+if ( username == dirusername[d] && password == dirpassword[d]){
+alert ("Login successfully");
+window.location = "Aboutus.html";
+}
+}
+}
