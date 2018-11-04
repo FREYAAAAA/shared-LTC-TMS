3d8831e1118614441db05fb77ee02f3c12ac3208
WangShuChen <37951295+WangShuChen@users.noreply.github.com>
2018-11-04 22:14:20 +0800

    hn

--- LTCTMS/IT/Feedback.js
+++ LTCTMS/IT/Feedback.js
@@ -1,91 +1,266 @@
-var fbFeedback = firebase.database().ref("Feedback/");
-var num = 0;
-fbFeedback.once("value")
-.then(function(snapshot){
-    snapshot.forEach(function(childSnapshot1){
-           match_id(childSnapshot1.key,fbFeedback);
-   });
-});
+window.onload=function(){
+    var fbFeedback = firebase.database().ref("Feedback/");
+    var num = 0;
+    fbFeedback.once("value")
+    .then(function(snapshot){
+        snapshot.forEach(function(childSnapshot1){
+            var id = childSnapshot1.key;
+            match_id(id,fbFeedback);
+       });
+    });
+}
+function match_id(id,fbFeedback){
 
-function match_id(id){
-    console.log(id);
     if (id.charAt(0) == "3"){
-        var fbCNA = firebase.database().ref("CNA/"+id+"/Portfolio/Name");
+        var fbCNA = firebase.database().ref("CNA/"+id+"/Portfolio");
         let arr_name = [];
         var name;
-         fbCNA.once("value")
-            .then(function(snapshot){
-                name = snapshot.val();
-                tableform(id,name,fbFeedback);
-            });
+        var picture;
+        fbCNA.once("value")
+           .then(function(snapshot){
+               var name;
+               var picture;
+               snapshot.forEach(function(childSnapshot1){
+                   if(childSnapshot1.key == "Name"){
+                       name = childSnapshot1.val();
+                   }
+                   if(childSnapshot1.key =="pictureurl"){
+                       picture = childSnapshot1.val();
+                       tableform(id,name,fbFeedback,picture);
+                   }
+               })
+           });
     }
     else{
-        var fbCNA = firebase.database().ref("Patient/"+id+"/Portfolio/Name");
+        var fbPAT = firebase.database().ref("Patient/"+id+"/Portfolio");
         let arr_name = [];
         var name;
-         fbCNA.once("value")
-            .then(function(snapshot){
-                name = snapshot.val();
-                tableform(id,name,fbFeedback);
-            });
+        fbPAT.once("value")
+           .then(function(snapshot){
+               var name;
+               var picture;
+               snapshot.forEach(function(childSnapshot1){
+                   if(childSnapshot1.key == "Name"){
+                       name = childSnapshot1.val();
+                   }
+                   if(childSnapshot1.key =="pictureurl"){
+                       picture = childSnapshot1.val();
+                       tableform(id,name,fbFeedback,picture);
+                   }
+               })
+           });
     }
 
 }
-
-function tableform(id,name,fbFeedback){
-    var index = 0;
+var index = 0;
+function tableform(id,name,fbFeedback,picture){
     fbFeedback.child(id).once('value').
     then(function(childSnapshot2){
         childSnapshot2.forEach(function(childSnapshot3 ){
+            var m = childSnapshot3.key;
+            var a = m.split("-");
+            var month = a[0]+"-"+a[1];
             childSnapshot3.forEach(function(childSnapshot4){
-                if(id.charAt(0) == "3"){
-                    document.getElementById("comment").innerHTML = childSnapshot4.val();
-                    document.getElementById("usename").innerHTML = name;
-                //    document.getElementById("user-fa").src = "/images/user_info_bg.jpg";
-                    var container = document.getElementById('container');
-                    var div = document.createElement('div');
-                    var span = document.createElement('span');
-                    var span1 = document.createElement('span');
-                    var i = document.createElement("i");
-                    var div1 = document.createElement('div');
-                    var span2 = document.createElement('span');
-                    div.setAttribute("class", "liuyan");
-                    span.setAttribute("id", "user-fa["+index+"]");
-                    i.setAttribute("class", "fa fa-user");
-                    div1.setAttribute("id","user["+index+"]");
-                    span2.setAttribute("id", "comment["+index+"]");
-                    span1.setAttribute("id","username["+index+"]");
+                var t = childSnapshot4.key;
+                var time = t.split("-");
+                var time1 = time[1].split(":");
+                var value = childSnapshot4.val();
+            //  document.getElementById("user-fa").src = "/images/user_info_bg.jpg";
+                var container = document.getElementById('container');
+                var div = document.createElement('div');
+                var span0 = document.createElement("span");
+                var span = document.createElement('span');
+                var span1 = document.createElement('span');
+                var i = document.createElement("i");
+                var img = document.createElement("img");
+                var div1 = document.createElement('div');
+                var span2 = document.createElement('span');
+                var span3 = document.createElement("span");
+                var span4 = document.createElement("span");
 
-                    container.appendChild(div);
-                    div.appendChild(span);
-                    div.appendChild(div1);
-                    div1.appendChild(span1);
-                    div.appendChild(span2);
+                var div2 = document.createElement('div');
+                var div3 = document.createElement('div');
+                var input = document.createElement("input");
+                var button = document.createElement("button");
+                div.setAttribute("class", "liuyan");
+                div.setAttribute("id", "liuyan["+index+"]");
+                span0.setAttribute("id","time["+index+"]");
+                span0.setAttribute("class","time");
+                span.setAttribute("id", "user-fa["+index+"]");
+                i.setAttribute("class", "fa fa-user");
+                img.setAttribute("class", "user-fa");
+                img.setAttribute("id", "photo["+index+"]");
+                //img.setAttribute("src","#");
+                div1.setAttribute("id","user["+index+"]");
+                span2.setAttribute("id", "comment["+index+"]");
+                span2.setAttribute("class", "comment");
+                span1.setAttribute("id","username["+index+"]");
+                span3.setAttribute("onclick","replyToggle("+index+")");
+                span3.setAttribute("class", "reply");
+                span3.setAttribute("id","reply["+index+"]")
+                span4.setAttribute("id","replyComment["+index+"]");
 
-                    document.getElementById("user-fa["+index+"]").href = "statics/images/user_info_bg.jpg";
-                    document.getElementById("username["+index+"]").innerHTML = name +"  said:";
-                    document.getElementById("comment["+index+"]").innerHTML = childSnapshot4.val();
+                div2.setAttribute("id" ,"usermessage");
+                div3.setAttribute("class" ,"content");
+                input.setAttribute("id", "message["+index+"]");
+                input.setAttribute("class", "text-success");
+                input.setAttribute("type", "text");
+                button.setAttribute("id","btn["+index+"]");
+                button.setAttribute("class", "btn-success");
 
-                    index++;
-                    var row = CNA_Feedback.insertRow(-1);
-                    var cellID = row.insertCell(-1);
-                    var cellName = row.insertCell(-1);
-                    var cellComment = row.insertCell(-1);
-                    cellID.appendChild(document.createTextNode(id));
-                    cellName.appendChild(document.createTextNode(name));
-                    cellComment.appendChild(document.createTextNode(childSnapshot4.val()));
+                button.setAttribute("onclick", "sendMess("+id+","+a[0]+","+a[1]+","+index+","+time[0]+","+time1[0]+","+time1[1]+","+time1[2]+","+value+")");
+                button.innerHTML= "Enter";
+                span3.innerHTML = "Reply";
+
+                if(childSnapshot3.key != "Reply"){
+                    if(id.charAt(0) == "3"){
+                        container.appendChild(div);
+
+                        div.appendChild(span);
+                        span.appendChild(i);
+                        i.appendChild(img);
+                        div.appendChild(div1);
+                        div1.appendChild(span1);
+                        div.appendChild(span2);
+                        div.appendChild(span3);
+                        div.appendChild(span0);
+
+
+
+                        div.appendChild(div2);
+                        div2.appendChild(div3);
+                        div2.appendChild(span4);
+                        div3.appendChild(input);
+                        div3.appendChild(button);
+
+                        document.getElementById("time["+index+"]").innerHTML = month+"-"+t;
+                        document.getElementById("photo["+index+"]").src = picture;
+                        document.getElementById("username["+index+"]").innerHTML = name +"  said:";
+                        document.getElementById("comment["+index+"]").innerHTML = childSnapshot4.val();
+                        getReply(id,index,a[0],a[1],time[0],time1[0],time1[1],time1[2]);
+                        console.log(index);
+                        index++;
+                    }
+                    else{
+                        //var container1 = document.getElementById("container1");
+                        container.appendChild(div);
+                        div.appendChild(span);
+                        span.appendChild(i);
+                        i.appendChild(img);
+                        div.appendChild(div1);
+                        div1.appendChild(span1);
+                        div.appendChild(span2);
+                        div.appendChild(span3);
+                        div.appendChild(span0);
+
+
+                        div.appendChild(div2);
+                        div2.appendChild(div3);
+                        div2.appendChild(span4);
+                        div3.appendChild(input);
+                        div3.appendChild(button);
+
+                        document.getElementById("time["+index+"]").innerHTML = month+"-"+t;
+                        document.getElementById("photo["+index+"]").src = picture;
+                        document.getElementById("username["+index+"]").innerHTML = name +"  said:";
+                        document.getElementById("comment["+index+"]").innerHTML = childSnapshot4.val();
+
+                        getReply(id,index,a[0],a[1],time[0],time1[0],time1[1],time1[2]);
+                        console.log(index);
+
+                    }
                 }
-                else{
-                    var row = Patient_Feedback.insertRow(-1);
-                    var cellID = row.insertCell(-1);
-                    var cellName = row.insertCell(-1);
-                    var cellComment = row.insertCell(-1);
-                    cellID.appendChild(document.createTextNode(id));
-                    cellName.appendChild(document.createTextNode(name));
-                    cellComment.appendChild(document.createTextNode(childSnapshot4.val()));
 
-                }
             })
-        });
+        })
     });
+
+}
+function getReply(id,index,year,month,date,h,m,s){
+    var key = year+"-"+month;
+    var time = date+"-"+h+":"+m+":"+s;
+    var fbReply = firebase.database().ref("Feedback/"+id+"/Reply"+"/"+key+"/"+time);
+    fbReply.once('value').
+    then(function(snapshot){
+        document.getElementById("replyComment["+index+"]").innerHTML = snapshot.val();
+    })
+}
+
+function replyToggle(index){
+    if(document.getElementById("message["+index+"]").style.display =="inline"){
+        document.getElementById("message["+index+"]").style.display = "none";
+        document.getElementById("reply["+index+"]").innerHTML = "Reply";
+        document.getElementById("btn["+index+"]").style.display = "none";
+
+    }
+    else{
+        document.getElementById("message["+index+"]").style.display = "inline";
+        document.getElementById("reply["+index+"]").innerHTML = "Collapse";
+        document.getElementById("btn["+index+"]").style.display = "inline";
+    }
+}
+
+function sendMess(id,year,month,index,date,h,m,s,value){
+    var key = year+"-"+month;
+    var idre = "000001"; // Replyer's  ID
+    var time = date+"-"+h+":"+m+":"+s;
+    var comment = document.getElementById("message["+index+"]").value;
+
+    firebase.database().ref("Feedback/"+id+"/Reply/"+key+"/"+"/"+time).set(comment);
+    document.getElementById("replyComment["+index+"]").innerHTML = comment;
+    replyToggle(index);
+    sortingList(index);
+    console.log(comment);
+
+}
+
+
+function sortingList(index){
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("container");
+  switching = true;
+   console.log(index);
+  dir = "asc";
+  while (switching) {
+    switching = false;
+    for (i = 0; i < 5; i++) {
+
+        rows = document.getElementById("liuyan["+i+"]");
+        shouldSwitch = false;
+        console.log(rows);
+        x = rows.getElementsByTagName("span")[4];
+        var c = i+1;
+        var rows1 = document.getElementById("liuyan["+c+"]");
+        y = rows1.getElementsByTagName("span")[4];
+        console.log(rows1);
+        console.log(dir);
+
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch= true;
+          console.log("please stop");
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          console.log("please stop1");
+
+          break;
+        }
+      }
+    }
+    break;
+    if (shouldSwitch) {
+      rows.parentNode.insertBefore(rows1, rows);
+      console.log("suc");
+      switching = true;
+      switchcount ++;
+    } else {
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
 }
