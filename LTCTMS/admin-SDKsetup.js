a8d944c191c729a92ae120d9a62cbad09790daf2
Gashima3 <37646616+Gashima3@users.noreply.github.com>
2018-11-08 14:05:32 +0800

    l;ll

--- LTCTMS/admin-SDKsetup.js
+++
@@ -1,9 +0,0 @@
-
-var admin = require("firebase-admin");
-
-var serviceAccount = require("C:\inetpub\wwwroot\LTCTMS\share-b7589-firebase-adminsdk-9gtpk-8d7012282d.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://share-b7589.firebaseio.com"
-});
