8df7ce4b45cd125f62f6645ed2f59defe89d7cd8
Gashima3 <37646616+Gashima3@users.noreply.github.com>
2018-11-12 14:52:20 +0800

    qweq

--- LTCTMS/IT/Server.js
+++ LTCTMS/IT/Server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Firebase DB
 var firebase=require("firebase");
 var config = {
@@ -51,57 +50,3 @@ exports.createUser= functions.database.ref('uAccount/{position}/{sid}').onUpdate
 });
 
 /**/
-=======
-//Firebase DB
-var firebase=require("firebase");
-var config = {
-   apiKey: "AIzaSyCnuAZzFvkT-FSRxB5Vk67JM6FU9wZLYMQ",
-   authDomain: "share-b7589.firebaseapp.com",
-   databaseURL: "https://share-b7589.firebaseio.com",
-   projectId: "share-b7589",
-   storageBucket: "share-b7589.appspot.com",
-   messagingSenderId: "323469467975"
- };
- firebase.initializeApp(config);
-
-//Admin SDK setup
-var admin = require("firebase-admin");
-
-var serviceAccount = require("/Users/Gama/Documents/GitHub/shared-LTC-TMS/LTCTMS/share-b7589-firebase-adminsdk-9gtpk-8d7012282d");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://share-b7589.firebaseio.com"
-});
-
-//Functions
-var functions = require("firebase-functions");
-
-exports.createUser= functions.database.ref('uAccount/{position}/{sid}').onUpdate((snapshot, context)=>{
-  var wp = context.params.position;
-  var id = context.params.sid;
-  console.log('wp ='+ wp + 'id ='+ id);
-  var childData = snap.val();
-
-  admin.auth().createUser({
-      email: childData.Email,
-      password:childData.Password,
-      displayName:childData.Name,
-      disabled:false,
-      emailVerified: true
-  }).then(function(userRecord){
-    console.log("user", userRecord.toJSON());
-    var updates={};
-    var userData=userRecord.toJSON();
-    console.log(userData);
-    var updatedList = JSON.parse(JSON.stringify(userData));
-    console.log(updatedList);
-    updates['uAccount/'+childData.Position+'/Account']= updatedList;
-    firebase.database().ref().update(updates);
-  }).catch(function(error){
-    console.log(error.message);
-  });
-});
-
-/**/
->>>>>>> 8a5188f3bcbf48625a2bee5a5a9440519e64be9e
