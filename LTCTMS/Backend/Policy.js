aa722752010bd3f5d6799f1703baa80897bddd6e
Gashima3 <37646616+Gashima3@users.noreply.github.com>
2018-11-15 15:09:08 +0800

    wqqqq

--- LTCTMS/Backend/Policy.js
+++ LTCTMS/Backend/Policy.js
@@ -1,3 +1,20 @@
+firebase.auth().onAuthStateChanged(function (firebaseUser){
+if(firebaseUser){
+  console.log(firebaseUser);
+  var userid = firebaseUser.uid;
+  var displayName = firebaseUser.displayName;
+  console.log(displayName);
+  console.log(userid);
+}else{
+  alert("You're Logged out now! Please Login again if you need to use this system!");
+  window.location.href = "//C:/Users/Gama/Documents/GitHub/shared-LTC-TMS/LTCTMS/Frontend/00Login2.html";
+}
+});
+
+function Logout(){
+  firebase.auth().signOut();
+}
+
 var storageRef = firebase.storage().ref('Policy/policy.html');
   storageRef.getDownloadURL().then(function (url) {
   firebase.database().ref("Policy/P1/System Policy/").set(url);
